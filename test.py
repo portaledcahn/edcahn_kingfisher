c2fbb7cac4087323aafd37cff407a28f560fd823
Portal EDCA HN <50455339+portaledcahn@users.noreply.github.com>
2020-06-18 08:29:01 -0600

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import PublicadoresTareas
-
-x = 1
-PublicadoresTareas.PublicadoresTareas("HN.SEFIN").ejecutar()
-#PublicadoresTareas.PublicadoresTareas("HN.ONCAE").ejecutar()
-#PublicadoresTareas.PublicadoresTareas("HN.ONCAE").ejecutar()
-
